27b9f568c0697f010df8888a7293e54ca57e6d8c
taeyeongKims <137798168+taeyeongKims@users.noreply.github.com>
2023-07-30 19:11:40 +0900

    Add files via upload

--- serverCrawling.js
+++ serverCrawling.js
@@ -1,8 +1,12 @@
-const express = require('express')
+const express = require('express');
 const app = express()
 const cors = require('cors')
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+
+let sessionData;
+
+startServer();
 
 app.use(cors())
 app.get('/', (req, res) =>{
@@ -12,67 +16,23 @@ app.get('/', (req, res) =>{
 app.get('/place', async(req, res) =>{
   const selected_place = req.query.select_place;
   const selected_date = req.query.valueToFind;
-  console.log(selected_date);
 
-  if(selected_place == "gangseo1"){
-    order = 4;
-  } else if(selected_place == "gangseo2"){
-      order = 3;
-  } else if(selected_place == "gangseo3"){
-      order = 2;
-  } else if(selected_place == "gangseo4"){
-      order = 1;  
-  } else if(selected_place == "macdo"){
-      order = 5;
-  } else if(selected_place == "daejeo"){
-      order = 6;
-  } else if(selected_place == "hwamyeong"){
-      order = 7;
-  } else if(selected_place == "samnak"){
-      order = 8;
-  } else if(selected_place == "gudeok1"){
-      order = 9;
-  } else if(selected_place == "gudeok2"){
-      order = 10;
-  } else if(selected_place == "gudeok3"){
-      order = 11;
-  } else if(selected_place == "spoonePark"){
+  if(selected_place == "spoonePark"){
       res.json({'place' : "스포원"});
   } else if(selected_place == "sapryang"){
       res.json({'place' : "삽량"});
   } else if(selected_place == "sajik"){
       res.json({'place' : "사직구장"});
   }
-  let tennisTime = await reserveParsing(order, selected_date);
+  let tennisTime = await reserveParsing(selected_place, selected_date);
    res.json({'time' :tennisTime});
 })
 
 app.get('/schedule', async (req, res) => {
     const selected_place = req.query.select_place;
+    const calendarMonth = req.query.Calendar;
 
-  if(selected_place == "gangseo1"){
-    order = 4;
-  } else if(selected_place == "gangseo2"){
-      order = 3;
-  } else if(selected_place == "gangseo3"){
-      order = 2;
-  } else if(selected_place == "gangseo4"){
-      order = 1;  
-  } else if(selected_place == "macdo"){
-      order = 5;
-  } else if(selected_place == "daejeo"){
-      order = 6;
-  } else if(selected_place == "hwamyeong"){
-      order = 7;
-  } else if(selected_place == "samnak"){
-      order = 8;
-  } else if(selected_place == "gudeok1"){
-      order = 9;
-  } else if(selected_place == "gudeok2"){
-      order = 10;
-  } else if(selected_place == "gudeok3"){
-      order = 11;
-  } else if(selected_place == "spoonePark"){
+  if(selected_place == "spoonePark"){
       res.json({'place' : "스포원"});
   } else if(selected_place == "sapryang"){
       res.json({'place' : "삽량"});
@@ -80,7 +40,7 @@ app.get('/schedule', async (req, res) => {
       res.json({'place' : "사직구장"});
   }
     try {
-        let $tableCalender = await scheduleParsing(order);
+        let $tableCalender = await scheduleParsing(selected_place, calendarMonth);
         res.send($tableCalender);
     } catch (error) {
         console.error(error);
@@ -88,49 +48,95 @@ app.get('/schedule', async (req, res) => {
     }
   });
 
-app.listen(3000)
-
-async function reserveParsing(order, valueToFind) {
-  const browser = await puppeteer.launch({ headless: false});
-  const page = await browser.newPage();
-  await page.goto("https://reserve.busan.go.kr/rent"); // 이동 페이지 URL
+  async function startServer() {
+    // 로그인 및 세션 정보 얻기
+    sessionData = await openWebPage();
   
-  await page.evaluate(() => {
-      const el = document.querySelector(".header-inner > ul >li > a");
-      el.click();
-  });
+    // 서버 시작
+    app.listen(3000, () => {
+      console.log('Server started');
+    });
+  }
+
+ //로그인 및 세션 정보 얻기 
+async function openWebPage() {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+
+  // 로그인 페이지로 이동
+  await page.goto('https://reserve.busan.go.kr/rent');
+  
+  // 로그인 동작 수행
+  await page.evaluate((username, password) => {
+    const el = document.querySelector('.header-inner > ul > li > a');
+    el.click();
+  })
   await page.waitForSelector('#mberId');
   await page.type('#mberId', 'xodud8530');
   await page.type('#mberPwd', 'qwpo1209!');
-
   await page.click('.btnLogin');
 
+  // 로그인 후 로그인 성공 여부를 확인하여 세션 정보 반환
   await page.waitForNavigation();
+  const sessionData = await page.cookies();
+  
+  // 브라우저 종료
+  await browser.close();
+  
+  return sessionData;
+}
 
-  await page.goto("https://reserve.busan.go.kr/rent/list?resveGroupSn=&progrmSn=&srchGugun=&srchResveInsttCd=&srchBeginDe=&srchEndDe=&srchIngStat=&srchVal=%ED%85%8C%EB%8B%88%EC%8A%A4%EC%9E%A5");
+async function reserveParsing(selected_place, valueToFind) {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.setCookie(...sessionData);
+  
+  page.setRequestInterception(true);
 
-//   await page.waitForSelector(".reserve-map-list > li:nth-child(2)");
-//   await page.click(".reserve-map-list > li:nth-child(3)");d
-//   await page.click(".reserve-map-submit")
+  // request 이벤트를 가로채고 필요한 리소스만 요청하도록 설정합니다.
+  page.on('request', (request) => {
+    const resourceType = request.resourceType();
 
-//   await page.waitForSelector('#srchVal');
-//   await page.type('#srchVal', "테니스장");
-//   await page.click('.btnInpType1');
+    // CSS, Script, XHR, Fetch 리소스 유형만 허용
+    if (
+      resourceType !== 'document' &&
+      resourceType !== 'script' &&
+      resourceType !== 'xhr' &&
+      resourceType !== 'fetch'
+    ) {
+      request.abort(); // 불필요한 리소스 요청 중단
+    } else {
+      request.continue(); // 필요한 리소스 요청 진행
+    }
+  });
 
-  if(order < 11){
-    await page.waitForSelector(`.reserveList > li:nth-of-type(${order})`);
-    await page.click(`.reserveList > li:nth-of-type(${order})`);
-  }else{
-    await page.waitForSelector('.number > a');
-    await page.click('.number > a');
-    await page.waitForSelector('.reserveList > li');
-    await page.click(`.reserveList > li`);
+  let url;
+  
+  if(selected_place == "gangseo1"){
+    url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=498&progrmSn=342";
+  } else if(selected_place == "gangseo2"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=498&progrmSn=343";
+  } else if(selected_place == "gangseo3"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=498&progrmSn=344";
+  } else if(selected_place == "gangseo4"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=498&progrmSn=345";  
+  } else if(selected_place == "macdo"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=55&progrmSn=219";
+  } else if(selected_place == "daejeo"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=55&progrmSn=217";
+  } else if(selected_place == "hwamyeong"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=55&progrmSn=214";
+  } else if(selected_place == "samnak"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=498&progrmSn=342";// url 바꿔야함
+  } else if(selected_place == "gudeok1"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=475&progrmSn=313";
+  } else if(selected_place == "gudeok2"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=475&progrmSn=312";
+  } else if(selected_place == "gudeok3"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=475&progrmSn=311";
   }
 
-  await page.waitForSelector(".btnTypeXL ");
-  await page.click(".btnTypeXL ");
-
-
+  await page.goto(url);
 
   await page.waitForSelector(".selectDay");
   const links = await page.$$('.selectDay > a'); // 모든 a 태그 선택
@@ -144,7 +150,6 @@ async function reserveParsing(order, valueToFind) {
       break; // 반복문 종료
     }
   }
-
   if (foundIndex !== -1) {
     const desiredChild = links[foundIndex];
     await desiredChild.click();
@@ -152,61 +157,81 @@ async function reserveParsing(order, valueToFind) {
 
   await page.waitForSelector("#beginTime");
 
-
-
   let courses = await timeParsing(page);
-  browser.close();
+  // browser.close();
   return courses;
 }
 
-async function scheduleParsing(order) {
-    const browser = await puppeteer.launch({ headless: false});
-    const page = await browser.newPage();
-    await page.goto("https://reserve.busan.go.kr/rent"); // 이동 페이지 URL
-    
-    await page.evaluate(() => {
-        const el = document.querySelector(".header-inner > ul >li > a");
-        el.click();
-    });
-    await page.waitForSelector('#mberId');
-    await page.type('#mberId', 'xodud8530');
-    await page.type('#mberPwd', 'qwpo1209!');
-  
-    await page.click('.btnLogin');
-  
-    await page.waitForNavigation();
-  
-    await page.goto("https://reserve.busan.go.kr/rent/list?resveGroupSn=&progrmSn=&srchGugun=&srchResveInsttCd=&srchBeginDe=&srchEndDe=&srchIngStat=&srchVal=%ED%85%8C%EB%8B%88%EC%8A%A4%EC%9E%A5");
-  
-  //   await page.waitForSelector(".reserve-map-list > li:nth-child(2)");
-  //   await page.click(".reserve-map-list > li:nth-child(3)");d
-  //   await page.click(".reserve-map-submit")
-  
-  //   await page.waitForSelector('#srchVal');
-  //   await page.type('#srchVal', "테니스장");
-  //   await page.click('.btnInpType1');
-  
-    if(order < 11){
-      await page.waitForSelector(`.reserveList > li:nth-of-type(${order})`);
-      await page.click(`.reserveList > li:nth-of-type(${order})`);
-    }else{
-      await page.waitForSelector('.number > a');
-      await page.click('.number > a');
-      await page.waitForSelector('.reserveList > li');
-      await page.click(`.reserveList > li`);
+async function scheduleParsing(selected_place, calendarMonth) {
+  const browser = await puppeteer.launch({headless: false});
+  const page = await browser.newPage();
+  await page.setCookie(...sessionData);
+
+  page.setRequestInterception(true);
+
+  // request 이벤트를 가로채고 필요한 리소스만 요청하도록 설정합니다.
+  page.on('request', (request) => {
+    const resourceType = request.resourceType();
+
+    // CSS, Script, XHR, Fetch 리소스 유형만 허용
+    if (
+      resourceType !== 'document' &&
+      resourceType !== 'script' &&
+      resourceType !== 'xhr' &&
+      resourceType !== 'fetch'
+    ) {
+      request.abort(); // 불필요한 리소스 요청 중단
+    } else {
+      request.continue(); // 필요한 리소스 요청 진행
     }
-  
-    await page.waitForSelector(".btnTypeXL ");
-    await page.click(".btnTypeXL ");
-  
-  
-  
-    await page.waitForSelector(".selectDay");
-  
-    let $tableCalender = await calenderParsing(page);
-    browser.close();
-    return $tableCalender;
+  });
+
+  let url;
+  // 이동 페이지 URL
+  if(selected_place == "gangseo1"){
+    url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=498&progrmSn=342";
+  } else if(selected_place == "gangseo2"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=498&progrmSn=343";
+  } else if(selected_place == "gangseo3"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=498&progrmSn=344";
+  } else if(selected_place == "gangseo4"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=498&progrmSn=345";  
+  } else if(selected_place == "macdo"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=55&progrmSn=219";
+  } else if(selected_place == "daejeo"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=55&progrmSn=217";
+  } else if(selected_place == "hwamyeong"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=55&progrmSn=214";
+  } else if(selected_place == "samnak"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=498&progrmSn=342";// url 바꿔야함
+  } else if(selected_place == "gudeok1"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=475&progrmSn=313";
+  } else if(selected_place == "gudeok2"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=475&progrmSn=312";
+  } else if(selected_place == "gudeok3"){
+      url = "https://reserve.busan.go.kr/rent/preStep?resveProgrmSe=R&resveGroupSn=475&progrmSn=311";
   }
+
+  await page.goto(url);
+  // ,{ waitUntil: 'domcontentloaded' }
+  await page.waitForSelector(".next");
+  await page.click(".next")
+  await page.waitForNavigation();
+
+  // if(calendarMonth == "prev"){
+  //   await page.waitForSelector(".prev");
+  //   await page.click('.prev');
+  // } 
+  // else if(calendarMonth == "next"){
+  //   await page.waitForSelector(".next");
+  //   await page.click('.next');
+  // }
+  // await page.waitForSelector(".selectDay");
+  
+  let $tableCalender = await calenderParsing(page);
+  // browser.close();
+  return $tableCalender;
+}
   
 
 async function timeParsing(page) {
@@ -226,10 +251,15 @@ async function timeParsing(page) {
 async function calenderParsing(page) {
     const html = await page.content();
     const $ = cheerio.load(html);
-    endDay = $(".endDay").find('a').text();
-    console.log(endDay);
-    $(".endDay").find('a').remove();
-    $(".endDay").text(endDay);
+    let endDay = [];
+    const $endDayList = $(".endDay");
+    $endDayList.each((index, node) => {
+      endDay[index] = $(node).text()
+  });
+  $(".endDay").find('a').remove();
+    $endDayList.each((index, node) => {
+    $(node).text(endDay[index]);
+});
     let $tableCalender = $(".tableCalendar.cal1").html();
 
     return $tableCalender;
